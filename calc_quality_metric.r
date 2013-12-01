d97f7fc606528a821004660e4d3195fa230f3b38
Stephanie Hyland <steph.hyland@gmail.com>
2013-12-01 17:46:56 -0500

    might as well put this on github!

---
+++ calc_quality_metric.r
@@ -0,0 +1,72 @@
+THRESHOLD<-0.01
+
+args<-commandArgs(TRUE)
+
+get_deriv_model<-function(dataset){
+    n<-nrow(dataset)
+    deriv<-vector(length=n)
+    prev_y<-0
+    prev_x<-0
+    before<-0
+    after<-0
+    crossed<-0
+    x_val<-0
+    y_val<-0
+    model<-NA
+    total_sorted<-sort(dataset[,4])
+    uniq_sorted<-dataset[,3][order(dataset[,4])]
+
+    for (i in seq(n)){
+        y<-uniq_sorted[i]
+        x<-total_sorted[i]
+        deriv[i]<-(y-prev_y)/(x-prev_x)
+        if ((deriv[i]<THRESHOLD)&(crossed==0)){
+            crossed<-1
+            before_x<-total_sorted[(i-1)]
+            after_x<-total_sorted[i]
+            before_y<-uniq_sorted[(i-1)]
+            after_y<-uniq_sorted[i]
+            x_val<-(0.5)*(after_x+before_x)
+            y_val<-(0.5)*(after_y+before_y)
+            }
+        prev_y<-y
+        prev_x<-x
+        }
+
+    if (x_val==0){
+        model<-lm(deriv[(n-5):n]~total_sorted[(n-5):n])
+        intercept<-summary(model)$coef[1]
+        slope<-summary(model)$coef[2]
+        x_val<-(THRESHOLD-intercept)/slope
+        real_intercept<-max(uniq_sorted)-slope*max(total_sorted)^2*(0.5)-intercept*max(total_sorted)
+        y_val<-(0.5)*slope*(x_val)^2+ intercept*x_val + real_intercept
+        }
+    return(list('deriv'=deriv,'x_val'=x_val,'y_val'=y_val,'lm'=model))
+    }
+
+the_data<-read.table(args[1],as.is=TRUE)
+plot_yn<-(args[2]=='1')
+data_path<-args[3]
+data_name<-args[4]
+
+deriv_data<-get_deriv_model(the_data)
+deriv<-deriv_data$deriv
+model<-deriv_data$lm
+x_metric<-deriv_data$x_val
+y_metric<-deriv_data$y_val
+cat(paste(x_metric,y_metric,'\n'))
+
+if(plot_yn){ cat('Creating quality plot!\n')
+    plot_path<-paste(data_path,'.metricplot.pdf',sep='')
+    pdf(plot_path,width=10)
+    plot(sort(the_data[,4])/1e6,the_data[order(the_data[,4]),3]/1e6,col='deepskyblue',xlim=c(0,max(max(the_data[,4]/1e6),1.03*(x_metric)/1e6)),ylim=c(0,max(max(the_data[,3]/1e6),1.03*(y_metric)/1e6)),xlab='# total reads (millions)',ylab='# unique positions (millions)',main=paste('Unique positions as a function of read depth (',data_name,')',sep=''),type='l')
+    points(the_data[,4]/1e6,the_data[,3]/1e6,col='deepskyblue',cex=0.5,pch=20)
+    segments(x_metric/1e6,-5,x_metric/1e6,y_metric/1e6,lty=2,col='deepskyblue')
+    segments(-50,y_metric/1e6,x_metric/1e6,y_metric/1e6,lty=2,col='deepskyblue')
+    if(!is.null(names(model))){
+        intercept<-max(the_data[,3])-coef(summary(model))[2]*max(the_data[,4])^2*(0.5)-coef(summary(model))[1]*max(the_data[,4])
+        curve((coef(summary(model))[2]*(0.5)*(1e6*x)^2)/1e6+coef(summary(model))[1]*(x) + intercept/1e6,from=max(the_data[,4])/1e6,to=x_metric/1e6,add=T,lty=2,col='deepskyblue')
+        }
+    graphics.off()
+    cat('Plot saved to', plot_path,'\n')
+   } 
---
+++ get_quality_metric.py
@@ -0,0 +1,81 @@
+#!/usr/bin/python
+# * ----------------------------------*
+# |  Script to get 'quality metric'   |
+# |  for data in bed format.          |
+# |                                   |
+# |  Stephanie Hyland, Nov 2013.      |
+# |  Tri-I CBM sh985@cornell.edu      |
+# * ----------------------------------*
+
+import random
+import gzip
+import sys
+import os
+import argparse
+from subprocess import call
+from copy import copy
+
+random.seed()
+
+parser = argparse.ArgumentParser(description='Calculate quality metric!')
+parser.add_argument('file',help='bed.gz file! (must be sorted - use sort-bed)',metavar='DATA.bed.gz')
+parser.add_argument('--plot',help='plot #unique v #total with (predicted) asymptote!',action='store_true')
+
+args = parser.parse_args()
+data_path = args.file
+plot_true = args.plot
+if plot_true:
+    plot = '1'
+else:
+    plot = '0'
+
+data_name = os.path.basename(data_path)
+record_path = data_path+'.record'
+recordfile = open(record_path,'w')
+
+master_dict = dict()
+loc_buffer = {'+': 0, '-': 0}
+n_uniq = {'+': 0, '-': 0}
+n_total = {'+': 0, '-': 0}
+
+frac_list = [float(frac)/40 for frac in range(1,40)]
+for frac in frac_list:
+    master_dict[frac] = (copy(loc_buffer),copy(n_uniq),copy(n_total))
+
+i=0
+for line in gzip.open(data_path):
+    if i%1000000==0:
+        print i
+    i+=1
+
+    if 'rRNA' in line:
+        continue
+
+    for frac in frac_list:
+        r = random.random()
+        if r<=frac:
+            strand = line.split()[5]
+            master_dict[frac][2][strand] +=1            # increasing the total count for this strand, for this frac
+            loc = int(line.split()[1])
+            if loc != master_dict[frac][0][strand]:     # is it the same as what was recorded previously? if no, register a unique hit!
+                master_dict[frac][1][strand]+=1         # increasing the unique count for this strand, for this frac
+                master_dict[frac][0][strand] = loc          # recording new location
+
+print 'Summary:'
+for frac in sorted(master_dict):
+    n_uniq = master_dict[frac][1]
+    n_total = master_dict[frac][2]
+    n_uniq_both=0
+    n_total_both=0
+    for strand in n_uniq:
+        print frac, strand, n_uniq[strand], n_total[strand], float(n_uniq[strand])/n_total[strand]
+        n_uniq_both = n_uniq[strand] + n_uniq_both
+        n_total_both = n_total[strand] + n_total_both
+    recordfile.write(data_name+'\t'+str(frac)+'\t'+str(n_uniq_both)+'\t'+str(n_total_both)+'\n')
+
+recordfile.close()
+
+script_dir = os.path.dirname(os.path.realpath(sys.argv[0]))
+R_script_path = script_dir+'/calc_quality_metric.r'
+Rscript = 'R --slave --file='+R_script_path+' --args '+record_path+' '+plot+' '+data_path+' '+data_name
+call(Rscript,shell=True)
